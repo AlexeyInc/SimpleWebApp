926dcbba2b21a156cc6bce978204e63f15f30115
Alexey <oleksii.ovsiannikov@1datagroup.com>
2020-09-23 21:40:48 +0300

    Created frontend

---
+++ frontend/src/Icons.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import user from './user.svg';
+
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core';
+
+export const UserIcon = () => (
+  <img
+    src={user}
+    alt="User"
+    css={css`
+      width: 12px;
+      opacity: 0.6;
+    `}
+    width="12px"
+  />
+);
