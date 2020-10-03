3f422ac16ea7e117f61d34f65c9990523ffb8a65
Alexey <oleksii.ovsiannikov@1datagroup.com>
2020-10-03 21:10:43 +0300

    Update QuestionPage.tsx
    Added import

--- frontend/src/QuestionPage.tsx
+++ frontend/src/QuestionPage.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react';
 import { Page } from './Page';
 import { RouteComponentProps } from 'react-router-dom';
+import { QuestionData, getQuestion } from './QuestionsData';
 
 interface RouteParams {
   questionId: string;
